1754edb72a515a8cc38cebf125bafcc894750d2a
Barri <info@bdesignstudio.co.uk>
2020-07-22 19:59:35 +0100

    Play with geolocation in the browser

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-
+//for deciding the season weather we need the latitude of the user
 const App = () => {
+  //making a call to the api
+  
+  window.navigator.geolocation.getCurrentPosition(
+    //to get the user location we need to set to separate function call back
+    //call back one success
+    (position)=>console.log(position),
+    // second call back -failure 
+    (err)=>console.log(err)
+  );
   return (
     <div>
      month
