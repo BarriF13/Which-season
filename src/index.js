1f507fe2342141c5f8c95c04b385b265b424386f
Barri <info@bdesignstudio.co.uk>
2020-07-23 11:33:55 +0100

    Handling Error

--- src/index.js
+++ src/index.js
@@ -24,20 +24,35 @@ import ReactDOM from 'react-dom'
 class App extends Component {
   constructor(props) {
      super(props);
-     //now we init state object- null = we don't know what is the latitude is yet.
-     this.state ={ lat: null };
+     // THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT to this.state 
+     //now we init state object- null = we don't know what the latitude is yet.
+     this.state ={ lat: null, errorMessage:'' };
+     //better to put the function when we call the app
+     window.navigator.geolocation.getCurrentPosition(
+
+      // (position) => console.log(position),
+      //we want to take latitude out of position object and using alone in state object above- so we use seState and we pass the object that has the update state that we want to make - in this case we want to use position object to update lat property 
+      // we call setState to update state object
+       (position) => {
+        this.setState({ lat: position.coords.latitude
+        })
+       },
+
+      (err) =>{
+        //  console.log(err);
+        this.setState({
+          errorMessage: err.message
+        })
+      }
+    );
   }
 
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-
-      (position) => console.log(position),
-
-      (err) => console.log(err)
-    );
+   //render func keep updating  so better to not having functions here
     return (
       <div>
-        Latitude:
+        Latitude:{this.state.lat}<br/>
+        Error: {this.state.errorMessage}
       </div>
     )
   }
