73580ca1c12ab16554c2c28b5c8a0607374daa75
Barri <info@bdesignstudio.co.uk>
2020-07-23 17:20:09 +0100

    add icon

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -2,22 +2,25 @@ import React from 'react'
 
 
 //function for finding the season
-const getSeason =( lat, month)=>{
- if(month > 2&& month<9 ){
-  return lat > 0 ? 'summer' : 'winter';
- } else {
-  return lat > 0 ? 'winter' : 'summer';
- }
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
 
 };
 
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth())
-
+  const text = season === 'winter' ? 'Good god, it is chilly' : 'Lovely summer time';
+  const icon = season === ' winter'? 'snowflake' : 'sun';
   return (
     <div>
-    <h1 style={{color: 'red'}}>{season === 'winter' ? 'Good god, it is chilly': 'Lovely summer time'}</h1> 
+      <i className={`${icon} icon`}/>
+      <h1 style={{ color: 'red' }}>{text}</h1>
+      <i className={`${icon} icon`}/>
     </div>
   );
 };
